cd9b529a7e8655a094952c59953c11d1418bde58
adityachandla <adityachandla@gmail.com>
2021-06-06 13:07:39 +0530

    Addition to table completed

--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module github.com/adityachandla/secondsProductService
 go 1.13
 
 require (
-	github.com/aws/aws-sdk-go v1.38.55
+	github.com/aws/aws-sdk-go-v2 v1.6.0 // indirect
 	github.com/aws/aws-sdk-go-v2/config v1.3.0
+	github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue v1.1.1 // indirect
 	github.com/aws/aws-sdk-go-v2/service/dynamodb v1.3.1
+	github.com/google/uuid v1.2.0 // indirect
 )
--- main.go
+++ main.go
@@ -3,22 +3,55 @@ package main
 import (
 	"context"
 	"fmt"
+	"math/rand"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/google/uuid"
 )
 
+const tablename = "Trial"
+
+type Column struct {
+	Key   string `dynamodbav:"key"`
+	Value string `dynamodbav:"value"`
+}
+
 func main() {
+	client := getClient()
+	for idx := 0; idx < 100; idx++ {
+		myValue := &Column{
+			Key:   uuid.New().String(),
+			Value: fmt.Sprintf("value -> %d", rand.Int31()),
+		}
+		addToTable(client, myValue)
+	}
+}
+
+func getClient() *dynamodb.Client {
 	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("us-east-1"))
 	if err != nil {
 		panic(err)
 	}
-	dynamoDbClient := dynamodb.NewFromConfig(cfg)
-	resp, err := dynamoDbClient.ListTables(context.TODO(), &dynamodb.ListTablesInput{})
+	return dynamodb.NewFromConfig(cfg)
+}
+
+func addToTable(client *dynamodb.Client, item *Column) {
+	av, err := attributevalue.MarshalMap(item)
 	if err != nil {
 		panic(err)
 	}
-	for _, name := range resp.TableNames {
-		fmt.Printf("Name: %s\n", name)
+
+	putItemInput := &dynamodb.PutItemInput{
+		Item:      av,
+		TableName: aws.String(tablename),
 	}
+
+	_, err = client.PutItem(context.TODO(), putItemInput)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Inserted item")
 }
